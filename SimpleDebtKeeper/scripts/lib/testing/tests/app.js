541faf5e1cf18b31bd3fdf9c348eb10266418a85
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2013-06-14 18:11:08 +0300

    Start dev using TDD

--- SimpleDebtKeeper/scripts/lib/testing/tests/app.js
+++ SimpleDebtKeeper/scripts/lib/testing/tests/app.js
@@ -1,7 +1,21 @@
 ﻿/// <reference path="~/scripts/lib/testing/qunit.js" />
 
-module("Dummy");
+(function () {
 
-test("QUnit Dummy Test", function () {
-    ok(1 === 1, "QUnit is working fine");
-});
+    module("Debts Basic Operations");
+
+    test("View current debts when no debts", function () {
+        var debts = getCurrentDebts();
+        ok(debts.length === 0, "OK, debts are empty");
+    });
+
+    test("View current debts when some debts exist", function () {
+        var debts = getCurrentDebts(function () { return [1]; });
+        ok(debts.length > 0, "OK, we have some debts");
+    });
+
+    function getCurrentDebts(debtsFactory) {
+        return typeof debtsFactory === "function" ? debtsFactory.call(this) : [];
+    }
+
+}).call(this);
