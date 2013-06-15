0863cc289d4e196554545805674ea6ccc8aad601
Hintea Dan Alexandru <hinteadan@gmail.com>
2013-06-15 21:57:55 +0300

    Some more unit tests plus a little refactoring

--- SimpleDebtKeeper/scripts/app.js
+++ SimpleDebtKeeper/scripts/app.js
@@ -1,8 +1,70 @@
 ﻿/// <reference path="~/scripts/lib/knockout.js" />
-/// <reference path="~/scripts/lib/jquery.js" />
 
 (function () {
 
-    
+    var rootScope = this;
+
+    function isFunction(f){
+        return typeof f === 'function';
+    }
+
+    function DataRepository(initialDataFactory) {
+
+        var data = [];
+
+        function construct() {
+            if (isFunction(initialDataFactory)) {
+                data = initialDataFactory.call(rootScope);
+            }
+        };
+
+        this.Add = function (item) {
+            data.push(item);
+        }
+
+        this.Remove = function (item) {
+            for (var index = 0; index < data.length; index++) {
+                if (data[index] !== item) {
+                    continue;
+                }
+                data.splice(index, 1);
+            }
+        }
+
+        this.Query = function () {
+            return data;
+        }
+
+        construct();
+    }
+
+    function AppModel() {
+        this.DebtType = {
+            PersonOwesYou: 0,
+            YouOwePerson: 1
+        };
+
+        this.Debt = function (person, type, amount, reason) {
+            this.Person = typeof person !== 'undefined' ? person : null;
+            this.Type = typeof type !== 'undefined' ? type : null;
+            this.Amount = typeof amount !== 'undefined' ? amount : null;
+            this.Reason = typeof reason !== 'undefined' ? reason : null;
+        };
+    }
+
+    function App(dataRepository) {
+
+        var dataRepository = dataRepository || new DataRepository();
+
+        this.Repository = function () {
+            return dataRepository;
+        }
+    }
+
+    this.App = {
+        AppModule: App,
+        Model: new AppModel()
+    };
+    this.DataRepository = DataRepository;
 
 }).call(this);
--- SimpleDebtKeeper/scripts/lib/testing/tests/app.js
+++ SimpleDebtKeeper/scripts/lib/testing/tests/app.js
@@ -1,21 +1,68 @@
 ﻿/// <reference path="~/scripts/lib/testing/qunit.js" />
+/// <reference path="~/scripts/lib/underscore.js" />
+/// <reference path="~/scripts/app.js" />
 
 (function () {
 
-    module("Debts Basic Operations");
+    var appWithEmptyRepository,
+        appWithSomeRepository;
+
+    module("Debts Basic Operations", {
+        setup: function () {
+            appWithEmptyRepository = new App.AppModule();
+            appWithSomeRepository = new App.AppModule(new DataRepository(function () {
+                return [new App.Model.Debt()];
+            }));
+        }
+    });
 
     test("View current debts when no debts", function () {
-        var debts = getCurrentDebts();
+        var debts = appWithEmptyRepository.Repository().Query();
         ok(debts.length === 0, "OK, debts are empty");
     });
 
     test("View current debts when some debts exist", function () {
-        var debts = getCurrentDebts(function () { return [1]; });
+        var debts = appWithSomeRepository.Repository().Query();
         ok(debts.length > 0, "OK, we have some debts");
     });
 
-    function getCurrentDebts(debtsFactory) {
-        return typeof debtsFactory === "function" ? debtsFactory.call(this) : [];
-    }
+    test("A debt has all mandatory properties", function () {
+        var debt = appWithSomeRepository.Repository().Query()[0];
+        ok(typeof debt.Type !== 'undefined', "Has Type property");
+        ok(typeof debt.Person !== 'undefined', "Has Person property");
+        ok(typeof debt.Amount !== 'undefined', "Has Amount property");
+        ok(typeof debt.Reason !== 'undefined', "Has Reason property");
+    });
+
+    module("Debts CRUD", {
+        setup: function () {
+            appWithEmptyRepository = new App.AppModule();
+            appWithSomeRepository = new App.AppModule(new DataRepository(function () {
+                return [new App.Model.Debt()];
+            }));
+        }
+    });
+
+    test("Add", function () {
+        var debts = appWithEmptyRepository.Repository().Query();
+        ok(debts.length === 0, "No debts at start");
+        appWithEmptyRepository.Repository().Add(
+            new App.Model.Debt("Dan", App.Model.DebtType.PersonOwesYou, 10, "Test")
+            );
+        ok(debts.length === 1, "New debt added");
+        ok(debts[0].Person === "Dan");
+        ok(debts[0].Amount === 10);
+        ok(debts[0].Reason === "Test");
+        ok(debts[0].Type === App.Model.DebtType.PersonOwesYou);
+    });
+
+    test("Delete", function () {
+        var debts = appWithSomeRepository.Repository().Query(),
+            debtToDelete = debts[0];
+
+        ok(debts.length > 0, "Some debts at start");
+        appWithSomeRepository.Repository().Remove(debtToDelete);
+        ok(!_(debts).contains(debtToDelete));
+    });
 
 }).call(this);
