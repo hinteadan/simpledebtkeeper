c976125c79c24e04215fbb7ef9d7f1a72a570465
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2013-06-18 13:08:37 +0300

    Factory library - synchronous method

---
+++ SimpleDebtKeeper/scripts/lib/testing/tests/factory.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="~/scripts/lib/testing/qunit.js" />
+/// <reference path="~/scripts/lib/underscore.js" />
+
+(function () {
+
+    module("Factory library");
+
+    test("Synchronous factory", function () {
+        var instance = factory(function () { return new dummyObject(); });
+
+        ok(typeof instance !== undefined, "Instance is created");
+        ok(instance.dummyObjectIdentifier !== undefined, "Instance is of expected type");
+    });
+
+    function dummyObject() {
+        this.dummyObjectIdentifier = -1;
+    }
+
+    function factory(creationFunc) {
+        return creationFunc.call(this);
+    }
+
+}).call(this);
