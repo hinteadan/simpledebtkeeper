fc31eae2efc619749fb50333146497e4c3216855
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2013-06-18 15:06:05 +0300

    Create the app data repository using the factory module

--- SimpleDebtKeeper/scripts/main.js
+++ SimpleDebtKeeper/scripts/main.js
@@ -1,14 +1,17 @@
 ﻿/// <reference path="~/scripts/lib/knockout.js" />
+/// <reference path="~/scripts/lib/factory.js" />
 /// <reference path="~/data.js" />
 /// <reference path="~/scripts/app.js" />
 /// <reference path="~/scripts/SummaryViewModel.js" />
 
-(function () {
+(function (create) {
 
     ko.applyBindings(
         new App.SummaryViewModel(
-            new App.AppModule(new DataRepository(function () { return DataSet; }))
+            new App.AppModule(new DataRepository(
+                create.sync(function () { return DataSet; }))
+            )
         )
     );
 
-}).call(this);
+}).call(this, new Factory());
