8a718b42af49ceab8b990f940a8eaa3eef5c9317
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2013-06-18 14:49:29 +0300

    factory function with unit tests

--- SimpleDebtKeeper/scripts/lib/testing/tests/factory.js
+++ SimpleDebtKeeper/scripts/lib/testing/tests/factory.js
@@ -12,8 +12,58 @@
         ok(instance.dummyObjectIdentifier !== undefined, "Instance is of expected type");
     });
 
+    asyncTest("Asynchronous factory", 2, function () {
+        var instance = asyncFactory(
+            function (onDone) {
+                setTimeout(function () {
+                    instance = new dummyObject();
+                    onDone.call(this);
+                }, 50)
+            },
+            function () {
+                ok(typeof instance !== undefined, "Instance is created");
+                ok(instance.dummyObjectIdentifier !== undefined, "Instance is of expected type");
+                start();
+            }
+            );
+    });
+
+    asyncTest("Asynchronous factory times out", 2, function () {
+        var instance = asyncFactory(
+            function (onDone) {
+                setTimeout(function () {
+                    instance = new dummyObject();
+                }, 50)
+            },
+            function () {
+                ok(typeof instance !== undefined, "Instance is created");
+                ok(instance.dummyObjectIdentifier !== undefined, "Instance is of expected type");
+                start();
+            }
+            );
+    });
+
     function dummyObject() {
         this.dummyObjectIdentifier = -1;
+    }
+
+    function asyncFactory(creationFunc, doneNotifierFunc) {
+
+        var result = {},
+            timeoutInMilliseconds = 1000,
+            doneNotifier = wrapDoneNotifierFunc(),
+            timeoutId = setTimeout(doneNotifier, timeoutInMilliseconds);
+
+        function wrapDoneNotifierFunc() {
+            return function () {
+                clearTimeout(timeoutId);
+                doneNotifierFunc.call(this);
+            };
+        }
+
+        creationFunc.call(this, doneNotifier);
+
+        return result;
     }
 
     function factory(creationFunc) {
