7004bce7677e2790a491870996f8b8cedf1142c4
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2013-06-20 15:41:28 +0300

    CSProj commit

---
+++ SimpleDebtKeeper/scripts/AppModel.js
@@ -0,0 +1,26 @@
+﻿(function () {
+
+    function AppModel() {
+
+        var self = this;
+
+        this.DebtType = {
+            PersonOwesYou: 0,
+            YouOwePerson: 1
+        };
+
+        function Debt() {
+
+            this.Person = '';
+            this.Description = '';
+            this.Amount = 0;
+            this.IsClosed = false;
+            this.Type = self.DebtType.PersonOwesYou;
+
+        }
+
+    }
+
+    this.AppModel = new AppModel();
+
+}).call(window);
---
+++ SimpleDebtKeeper/scripts/Data.js
@@ -0,0 +1,36 @@
+﻿(function () {
+
+    var data = [
+        {
+            Person: "Ioana",
+            Amount: 50,
+            Type: 0,
+            Description: "Bere",
+            IsClosed: false
+        },
+        {
+            Person: "Ioana",
+            Amount: 20,
+            Type: 1,
+            Description: "Lunch",
+            IsClosed: false
+        },
+        {
+            Person: "Iulian",
+            Amount: 25,
+            Type: 1,
+            Description: "Lunch",
+            IsClosed: false
+        },
+        {
+            Person: "Iulian",
+            Amount: 70,
+            Type: 0,
+            Description: "Lunch",
+            IsClosed: true
+        }
+    ];
+
+    this.Data = data;
+
+}).call(this);
